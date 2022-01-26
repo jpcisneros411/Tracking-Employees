bbf521fb899bed648310c3a19713a882a7af542b
Monet Man <88108211+jpcisneros411@users.noreply.github.com>
2022-01-25 21:41:42 -0600

    fixes for dependancies

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const inquirer = require("inquirer");
+//const cTable = require('console.table');
 
 //const employee = require("./eployee_db")
 
